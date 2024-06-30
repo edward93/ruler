90174781564e3b047be6e9b09e8ec4c2c2911c32
Edward <edward.chopuryan@gmail.com>
2024-06-30 19:26:05 -0400

    adds 3 mark components

--- demo/src/RulerPackage/Mark.Component.tsx
+++ demo/src/RulerPackage/Mark.Component.tsx
@@ -37,24 +37,92 @@ export const MajorMarkComponent = (props: {
 };
 
 /**
+ * Renders a Special mark
+ * @param props - MajorMark component props
+ */
+export const SpecialMarkComponent = (props: {
+  index: number;
+  position: number;
+  paddingTop: number;
+  options: MarkOptions;
+  color: string;
+  width: number;
+}) => {
+  const { index, position, paddingTop, options, color, width } = props;
+  // TODO: do not hard code
+  const markTopMargin = 15;
+
+  return (
+    <g key={position} transform={`translate(${position}, ${paddingTop})`}>
+      <text x={0} y={10} fill="white" textAnchor="middle" fontSize={`${options.fontSize}rem`}>
+        {index}
+      </text>
+      <g transform={`translate(0, ${markTopMargin})`}>
+        <line
+          x1={0}
+          y1={0}
+          x2={0}
+          y2={options.height}
+          className="rc-ruler-hash-mark rc-ruler-special-hash-mark"
+          stroke={color}
+          strokeWidth={width}
+        />
+      </g>
+    </g>
+  );
+};
+
+/**
+ * Renders a Special mark
+ * @param props - MajorMark component props
+ */
+export const MinorMarkComponent = (props: {
+  index: number;
+  position: number;
+  paddingTop: number;
+  options: MarkOptions;
+  color: string;
+  width: number;
+}) => {
+  const { index, position, paddingTop, options, color, width } = props;
+  // TODO: do not hard code
+  const markTopMargin = 15;
+
+  return (
+    <g key={position} transform={`translate(${position}, ${paddingTop})`}>
+      <text x={0} y={10} fill="white" textAnchor="middle" fontSize={`${options.fontSize}rem`}>
+        {index}
+      </text>
+      <g transform={`translate(0, ${markTopMargin})`}>
+        <line
+          x1={0}
+          y1={0}
+          x2={0}
+          y2={options.height}
+          className="rc-ruler-hash-mark rc-ruler-minor-hash-mark"
+          stroke={color}
+          strokeWidth={width}
+        />
+      </g>
+    </g>
+  );
+};
+
+/**
  * Renders a mark based on the index
  *
  * @param props - Mark Component props
  * @returns Either Major, Minor or Special Mark
  */
 const MarkComponent = (props: { position: number; index: number; config: RulerConfig }) => {
-  // TODO: do not hard code
-  const markTopMargin = 15;
-
+  // props destructuring
   const { index, position, config } = props;
   const [paddingTop, , ,] = config.padding;
 
   // marks options
   const { majorMark, specialMark, minorMark } = config.marksOptions;
 
-  // TODO: create 3 separate components
   if ((index - majorMark.offset) % majorMark.frequency === 0) {
-    // this is a major mark
     return (
       <MajorMarkComponent
         index={index}
@@ -67,41 +135,25 @@ const MarkComponent = (props: { position: number; index: number; config: RulerCo
     );
   } else if ((index - specialMark.offset) % specialMark.frequency === 0) {
     return (
-      <g key={position} transform={`translate(${position}, ${paddingTop})`}>
-        <text x={0} y={10} fill="white" textAnchor="middle" fontSize={`${specialMark.fontSize}rem`}>
-          {index % specialMark.frequency}
-        </text>
-        <g transform={`translate(0, ${markTopMargin})`}>
-          <line
-            x1={0}
-            y1={0}
-            x2={0}
-            y2={specialMark.height}
-            className="rc-ruler-hash-mark"
-            stroke={config.markingsColor}
-            strokeWidth={config.markWidth}
-          />
-        </g>
-      </g>
+      <SpecialMarkComponent
+        index={index}
+        position={position}
+        paddingTop={paddingTop}
+        options={specialMark}
+        color={config.markingsColor}
+        width={config.markWidth}
+      />
     );
   } else {
     return (
-      <g key={position} transform={`translate(${position}, ${paddingTop})`}>
-        <text x={0} y={10} fill="white" textAnchor="middle" fontSize={`${minorMark.fontSize}rem`}>
-          {index % majorMark.frequency}
-        </text>
-        <g transform={`translate(0, ${markTopMargin})`}>
-          <line
-            x1={0}
-            y1={0}
-            x2={0}
-            y2={minorMark.height}
-            className="rc-ruler-hash-mark"
-            stroke={config.markingsColor}
-            strokeWidth={config.markWidth}
-          />
-        </g>
-      </g>
+      <MinorMarkComponent
+        index={index}
+        position={position}
+        paddingTop={paddingTop}
+        options={minorMark}
+        color={config.markingsColor}
+        width={config.markWidth}
+      />
     );
   }
 };
